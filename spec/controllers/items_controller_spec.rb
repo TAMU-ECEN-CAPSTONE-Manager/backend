8815699ddb8e872e69de3fcb945ad3fb323643b0
Souryendu Das <souryendu@gmail.com>
2021-11-03 04:53:49 +0000

    TDD Log Output

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,6 +3,13 @@ require 'rails_helper'
 RSpec.describe ItemsController, type: :controller do
   render_views
   describe 'Inventory Management Page' do
+    it 'Data Populates properly into the inventory page' do
+      Item.create({:name => 'Stepper Motor', :category => 'Motors', :quantity => '5', :inInventory => '5', :description => 'brushless, synchronous electric motor that converts digital pulses to mechanical shaft rotation'})
+      get :index
+      expect(response).to be_successful
+      expect(response.body).to include("Stepper Motor")
+    end
+
     it 'Inventory displays all elements in order of database entry' do
       item1 = Item.create({:name => 'Stepper Motor', :category => 'Motors', :quantity => '5', :inInventory => '5', :description => 'brushless, synchronous electric motor that converts digital pulses to mechanical shaft rotation'})
       item2 = Item.create({:name => 'Raspberry Pi', :category => 'Microprocessors', :quantity => '3', :inInventory => '3', :description => 'low cost, credit-card sized computer that plugs into a computer monitor or TV'})
@@ -13,18 +20,11 @@ RSpec.describe ItemsController, type: :controller do
       expect(response.body.index("Stepper Motor") < response.body.index("Raspberry Pi"))
     end
 
-    it 'Update a new item to inventory page if item not present' do
+    it 'Should delete an inventory item based on parameters passed and then redirect to inventory page' do
       item = Item.create({:name => 'Raspberry Pi', :category => 'Microprocessors', :quantity => '3', :inInventory => '3', :description => 'brushless, synchronous electric motor that converts digital pulses to mechanical shaft rotation'})
-      params = ActionController::Parameters.new(id: item.id, item: { name: "Raspberry Pi", category: "Microprocessors", quantity: "3", inInventory: "3", description: "brushless, synchronous electric motor that converts digital pulses to mechanical shaft rotation" })
-      response.code.should == "200"
-      get :index
-      response.body = "Raspberry Pi"
-      expect(response.body).to include("Raspberry Pi")
-      response.body = "Microprocessors"
-      expect(response.body).to include("Microprocessors")
-      response.body = "brushless, synchronous electric motor that converts digital pulses to mechanical shaft rotation"
-      expect(response.body).to include("brushless, synchronous electric motor that converts digital pulses to mechanical shaft rotation")
-      response.code.should == "200"
+      get :destroy, params: { id: item.id }
+      response.code.should == "302"
+      response.should redirect_to(items_path)
     end
   end
 end
