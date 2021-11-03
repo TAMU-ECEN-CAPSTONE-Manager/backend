6c5d7a3811d4e203e8defe025befa95818dba314
Souryendu Das <souryendu@gmail.com>
2021-11-03 02:00:25 +0000

    CRUD Database setup for inventory

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,46 @@
 class ItemsController < ApplicationController
+  before_action :current_item, only: [:show, :edit, :update, :destroy]
+
   def index
-		@items = Item.all
+    @items = Item.all
   end
 
   def show
   end
 
   def new
+    @items = Item.new
+  end
+
+  def create
+    item = Item.create(item_params)
+    redirect_to item_path(item)
+  end
+
+  def destroy
+    @item.destroy
+    flash[:notice] = "#{@item.name} deleted"
+    redirect_to items_path
+  end
+
+  def update
+    @item.update(item_params)
+    flash[:notice] = "#{@item.name} was successfully updated"
+    redirect_to item_path(@item)
   end
 
   def edit
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :category, :quantity, :inInventory, :description)
+  end
+
+  def current_item
+    id = params[:id] 
+    @item = Item.find(id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/about'
   get 'welcome/studenthome'
   resources :items
+  get '/items/:id', to: 'items#show'
   
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
