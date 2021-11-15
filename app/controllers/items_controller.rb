b9b0e0b6605081747dde88643f23ef816860a9a3
Souryendu Das <souryendu@gmail.com>
2021-11-15 23:30:38 +0000

    Code pushed for Inventory Checking out and return of items features Basica validation inside code has also been implemented to check boundary conditions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :current_item, only: [:show, :edit, :update, :destroy]
+  before_action :issue_item, only: [:issue, :checkout]
+  before_action :return_item, only: [:return, :returning]
 
   def index
     @items = Item.all
@@ -32,10 +34,59 @@ class ItemsController < ApplicationController
   def edit
   end
 
+  def issue
+  end
+
+  def checkout
+    vars = request.query_parameters
+    total = vars['total'].to_i
+    checkout = vars['checkout'].to_i
+    if checkout > total
+      flash[:error] = "#{@item.name} is not possible to do. More number of items requested for checkout, than actually available in inventory"
+      redirect_to issue_item_path(@item)
+    end
+    @item.update(:inInventory => total - checkout)
+    redirect_to item_path(@item)
+  end
+
+  def return
+  end
+
+  def returning
+    vars = request.query_parameters
+    total = vars['quantity'].to_i
+    inventory = vars['inInventory'].to_i
+    returning = vars['return'].to_i
+    if returning + inventory > total
+      flash[:error] = "#{@item.name} exceeds the total number of items of this type, return exact number of items"
+      redirect_to issue_return_item_path(@item)
+    end
+    @item.update(:inInventory => returning + inventory)
+    redirect_to item_path(@item)
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :category, :quantity, :inInventory, :description)
+  end
+
+  def issue_params
+    params.require(:item).permit(:total, :checkout)
+  end
+
+  def issue_item
+    id = params[:id]
+    @item = Item.find(id)
+  end
+
+  def return_params
+    params.require(:item).permit(:quantity, :inInventory, :return)
+  end
+
+  def return_item
+    id = params[:id]
+    @item = Item.find(id)
   end
 
   def current_item
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'welcome/studenthome'
   resources :items
   get '/items/:id', to: 'items#show'
+  get '/items/:id/issue', to: 'items#issue', as: 'issue_item'
+  get '/items/:id/checkout', to: 'items#checkout', as: 'checkout_item'
+  get '/items/:id/return', to: 'items#return', as: 'return_item'
+  get '/items/:id/returning', to: 'items#returning', as: 'returning_item'
   
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
