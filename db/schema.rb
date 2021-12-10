1bdad6819369399f43e1b5753b3e1fb506f16005
Souryendu Das <bubunda@tamu.edu>
2021-12-10 16:31:17 -0600

    Sign in fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_210056) do
+ActiveRecord::Schema.define(version: 2021_12_09_235037) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2021_12_08_210056) do
     t.integer "quantity"
     t.integer "inInventory"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "model_for_my_apis", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "project_recos", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
