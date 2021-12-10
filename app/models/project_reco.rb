d27bbd0efdbd1f2df928f3a9465d6a536a6295ae
Souryendu Das <bubunda@tamu.edu>
2021-12-10 17:23:47 -0600

    API Call created

--- app/models/project_reco.rb
+++ app/models/project_reco.rb
@@ -1,14 +1,9 @@
 class ProjectReco < ApplicationRecord
-    require 'rest-client'
-
-    @url
-
-    def self.getData
-        response = RestClient(@url) #, { :content_type => :json })
-    end
-
-    def self.retrieve_results(parameter)
-        @url = "https://google.com"
-        return JSON.parse(ProjectReco.getData)
+    require 'uri'
+    require 'net/http'
+    def results(projectid)
+        uri = URI('http://0.0.0.0:5000/projectList')
+        uri.query = URI.encode_www_form(params)
+        res = Net::HTTP.get_response(uri) 
     end
 end
