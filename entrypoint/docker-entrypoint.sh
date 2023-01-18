b8ad74050aff61b9b5bb779e6915293c6fd77f69
vaidhyesh <vaidhyesh1@gmail.com>
2023-01-18 12:34:22 -0600

    Update docker-entrypoint.sh

--- entrypoint/docker-entrypoint.sh
+++ entrypoint/docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ -f tmp/pids/server.pid ]; then
 fi
 
 rails generate active_record:session_migration
-rails generate migration add_column_to_requestinvs url:string cost_per_item:decimal total_cost:decimal team_name:string issued:boolean
+#rails generate migration add_column_to_requestinvs url:string cost_per_item:decimal total_cost:decimal team_name:string issued:boolean
 rake db:migrate
 rake db:seed
 rails server -e production
