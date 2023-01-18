0ca6b78c5bf5781195fb77707c5451bc6f98e09e
vaidhyesh <vaidhyesh1@gmail.com>
2023-01-18 12:18:21 -0600

    Updates to make new domain work

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :active_record_store, :key => '_graph_app_session', domain: 'tamu.ecencapstone.xyz'
+Rails.application.config.session_store :active_record_store, :key => '_graph_app_session', domain: 'tamuecencapstone.net'
